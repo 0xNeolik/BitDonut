b77c60fa8c7cf026089992d893cdf8323c2860fe
0xNeolik <alvaro.teran@ontheuser.com>
2022-05-12 12:48:28 +0200

    Create new Request style

--- client/src/Components/Campaigns/Campaigns.js
+++ client/src/Components/Campaigns/Campaigns.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import factory from "../../Web3/factory";
 import NavBar from "../Layout/NavBar";
-
+import Logging from "../Logging/Logging";
 import "./Campaigns.css";
 
 function Campaings() {
   const [campaings, setCampaings] = useState([]);
+  let [isLoadding, setIsLoadding] = useState(true);
 
   async function fetchData() {
     const campaingsFromFetch = await factory.methods
@@ -14,6 +15,7 @@ function Campaings() {
       .call();
 
     setCampaings(campaingsFromFetch);
+    setIsLoadding(false);
   }
 
   useEffect(() => {
@@ -26,24 +28,30 @@ function Campaings() {
 
   return (
     <>
-      <NavBar />
-      <div id="display">
-        <Link to={"/newCampaign"} id="btn-new-campaing">
-          <button>Create a new Campaing </button>
-        </Link>
+      {isLoadding ? (
+        <Logging />
+      ) : (
+        <>
+          <NavBar />
+          <div id="display">
+            <Link to={"/newCampaign"} id="btn-new-campaing">
+              <button>Create a new Campaing </button>
+            </Link>
 
-        <div id="wrapper">
-          {campaings.map((campaing) => {
-            return (
-              <Link to={`${campaing}`} id="section-campaigns">
-                <div>
-                  <p>Campaing Hash {campaing}</p>
-                </div>
-              </Link>
-            );
-          })}
-        </div>
-      </div>
+            <div id="wrapper">
+              {campaings.map((campaing) => {
+                return (
+                  <Link to={`${campaing}`} id="section-campaigns">
+                    <div>
+                      <p>Campaing Hash {campaing}</p>
+                    </div>
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
--- client/src/Components/Requests/NewRequest.js
+++ client/src/Components/Requests/NewRequest.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import web3 from "../../Web3/web3";
 import Campaing from "../../Web3/campaing";
+import NavBar from "../Layout/NavBar";
 
 function NewRequest() {
   const [message, setMessage] = useState("");
@@ -28,41 +29,56 @@ function NewRequest() {
       setMessage(err.message);
     }
   };
-
   return (
     <>
-      <div>NewRequest</div>{" "}
-      <form onSubmit={handleSubmit}>
-        <div>
+      <NavBar />
+      <div id="new-request-bg">
+        <div id="form-new-request">
           <div>
-            <label>Description</label>
-            <input
-              onChange={(event) => setDescription(event.target.value)}
-              name="description"
-              type="text"
-            />
-          </div>
-          <div>
-            <label>Recipient</label>
-            <input
-              onChange={(event) => setRecipient(event.target.value)}
-              name="recipient"
-              type="text"
-            />
-          </div>
-          <div>
-            <label>Amount</label>
-            <input
-              onChange={(event) => setValue(event.target.value)}
-              name="value"
-              step="any"
-              type="number"
-            />
+            <h1>Create a new Request</h1>
+            <form onSubmit={handleSubmit}>
+              <p>
+                <label>Description</label>
+                <textarea
+                  onChange={(event) => setDescription(event.target.value)}
+                  name="description"
+                  id="description-request"
+                  type="text"
+                />
+              </p>
+              <p>
+                <label>Recipient</label>
+                <input
+                  onChange={(event) => setRecipient(event.target.value)}
+                  name="recipient"
+                  id="address-request"
+                  type="text"
+                />
+              </p>
+              <p>
+                <label>Amount in ETH</label>
+                <input
+                  onChange={(event) => setValue(event.target.value)}
+                  name="value"
+                  step="any"
+                  id="amount-request"
+                  type="number"
+                />
+              </p>
+              <button type="submit">Generate the Request</button>
+            </form>
           </div>
         </div>
-        <button type="submit">Contribute</button>
-      </form>
-      {message}
+        <p>
+          {!message ? (
+            <></>
+          ) : message == "Successful transaction" ? (
+            <div id="correct-transaction">{message}</div>
+          ) : (
+            <div id="error-transaction">{message}</div>
+          )}
+        </p>
+      </div>
     </>
   );
 }
